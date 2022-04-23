889005193ddb240f81db1793effff3cb1e0b3d09
user <mininet@cs640vm>
2022-04-22 23:23:33 -0500

    Separated Transport class/subclasses to separate file

---
+++ src/Transport.java
@@ -0,0 +1,93 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *  The Transport class handles the bulk of the TCP general
+ *  processing. Specifics are implemented via the handlePacket(DatagramPacket)
+ *  method of the implementing class.
+ */
+public abstract class Transport {
+    final protected int lp, rp; // local port, remote port
+    final protected String filename;
+    final protected int mtu;   // max transmission unit
+    final protected int sws;    // sliding window size
+    final protected List<Byte> buffer;
+
+    private long currentAck;
+
+    protected Transport(int lp, int rp, String filename, int mtu, int sws) {
+        this.lp = lp;
+        this.rp = rp;
+        this.filename = filename;
+        this.mtu = mtu;
+        this.sws = sws;
+        buffer = new ArrayList<>(); 
+    }
+
+    public boolean send() {
+        // TODO Loop while we have bytes to send / rcv that have not been acked
+        // each time loading the packet using the handlePacket method
+        // that is implemented in the Sender and Receiver class
+
+        // We need to print the following stats, which should be done here:
+        // * <snd/rcv> <time> <flag-list> seq-number> <number of bytes> <ack number>
+
+        TCPpacket p = new TCPpacket();
+        while (true) {
+            // work loop
+            if (p != null) handlePacket(p);
+            return false; // FIXME tmp break;
+        }
+
+        //return false;
+    }
+
+    protected long getCurrentAck() {
+        return currentAck;
+    }
+
+    public abstract TCPpacket handlePacket(TCPpacket p);
+    public abstract TCPpacket getInitPacket();
+
+
+    public static class Sender extends Transport {
+        final private String rip; // remote ip
+        // ArrayList buffer (protected)
+        // constructor fields (protected)
+
+        public Sender(int lp, int rp, String rip, String filename, int mtu, int sws) {
+            super(lp, rp, filename, mtu, sws);
+            this.rip = rip;
+        }
+
+        @Override
+        public TCPpacket handlePacket(TCPpacket p) {
+            return new TCPpacket(); // FIXME, take into account receieved packet p
+        }
+
+        @Override
+        public TCPpacket getInitPacket() {
+            // FIXME need to have specific init packet
+            return new TCPpacket();
+        }
+    }
+
+    public static class Receiver extends Transport {
+        // ArrayList buffer (protected)
+        // constructor fields (protected)
+
+        public Receiver(int lp, int rp, String filename, int mtu, int sws) {
+            super(lp, rp, filename, mtu, sws);
+        }
+
+        @Override
+        public TCPpacket handlePacket(TCPpacket p) {
+            return new TCPpacket(); // FIXME 
+        }
+
+        @Override
+        public TCPpacket getInitPacket() {
+            return null; // should be null as we do not init as receiver
+        }
+    }
+}
